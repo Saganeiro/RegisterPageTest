6dd238beea8a3bf7025813c482a0d18a32dad523
RBAMOUSER\saganowt <tomasz.saganowski@contractors.roche.com>
2018-01-23 13:40:22 +0100

    Preparing tests part2

---
+++ DiffImages/src/test/java/aem/qa/sanityrodip/tests/TestRodipPreprod.java
@@ -0,0 +1,60 @@
+package aem.qa.sanityrodip.tests;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Parameterized;
+
+import com.cognifide.qa.commons.CommonUtils;
+import com.cognifide.qa.commons.Global;
+import com.cognifide.qa.commons.Parameters;
+import com.cognifide.qa.commons.template.TestTemplate;
+
+@RunWith(Parameterized.class)
+public class TestRodipPreprod extends TestTemplate {
+
+	Parameters param;
+	static int count = 0;
+	
+	@Parameterized.Parameters
+	public static List<Parameters[]> primeNumbers() {
+		List<Parameters> urlList = CommonUtils.readLinksFromFileIntoObject("src/test/resources/" + TestRodipPreprod.class.getSimpleName() + ".txt");
+		
+		Parameters[][] urlArray = new Parameters[urlList.size()][];
+		
+		for (int i=0; i<urlList.size(); i++)
+		{
+			urlArray[i] = new Parameters[] {urlList.get(i)};
+		}
+
+		return Arrays.asList(urlArray);
+   }
+	
+	public TestRodipPreprod(Parameters param)
+	{
+		this.param = param;
+	}
+	
+	@Test
+	public void TestCase(){
+		count++;
+
+		long start = System.currentTimeMillis();		
+		
+		if (Global.SELENIUM_PREPARATION_MODE)
+		{
+			CommonUtils.cleanDirectory(patternPath);	
+			preparePatterns(param, patternPath, count);
+		}
+		else
+			performTest(param, count);
+		
+		long stop = System.currentTimeMillis();
+		
+		testRunTime = reporter.formatTestRunTime(stop-start);
+		testNumber = count;
+	}
+}
